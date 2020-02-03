50925837947899649b23d75964f59509986b91b0
dinoxas <dhassiotis07@gmail.com>
2020-02-03 14:42:01 +0000

    added more styling

--- app/index.js
+++ app/index.js
@@ -21,26 +21,29 @@ function App() {
 
   return (
     <React.Fragment>
-      <div className="">
-        <div className="hero is-light">
+      <div>
+        <div className="hero is-primary">
           <div className="hero-body">
-            <h1 className="title is-3">
+            <h1 className="title is-3 has-text-centered">
               Fibonacci Sequence &amp; Prime Number Calculator
             </h1>
-            <div className="buttons">
-              <button className="button is-medium" onClick={add10}>
-                <span>Add 10</span>
-                <span className="icon is-small">
-                  <FaPlusCircle size={18} />
-                </span>
-              </button>
-              <button className="button is-medium" onClick={handleReset}>
-                <span>Reset</span>
-                <span className="icon">
-                  <FaSync color="rgb(20, 20, 20)" size={18} />
-                </span>
-              </button>
-            </div>
+          </div>
+        </div>
+
+        <div className="hero-body">
+          <div className="buttons">
+            <button className="button is-medium" onClick={add10}>
+              <span>Add 10</span>
+              <span className="icon is-small">
+                <FaPlusCircle size={18} />
+              </span>
+            </button>
+            <button className="button is-medium" onClick={handleReset}>
+              <span>Reset</span>
+              <span className="icon">
+                <FaSync color="rgb(20, 20, 20)" size={18} />
+              </span>
+            </button>
           </div>
         </div>
         <div className="hero-body">
